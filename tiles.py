d8dcd5066cecb582d91a8fd21d315348150a36ba
Moses <mosesjuniorb@gmail.com>
2022-12-10 15:34:16 -0500

    changed a few cmds

--- tiles.py
+++ tiles.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,size):
+    def __init__(self, pos, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
         self.image.fill('grey')
