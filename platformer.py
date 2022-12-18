c7543b56a2e151984c798be8e9ca9c508d0b7a56
Moses <mosesjuniorb@gmail.com>
2022-12-18 14:16:37 -0500

    d

--- platformer.py
+++ platformer.py
@@ -1,8 +1,6 @@
 import pygame, sys
 from settings import *
-from level import *
-from player import *
-from tiles import *
+from level import Level
 
 pygame.init()
 BLACK = (0, 0, 0)
@@ -12,14 +10,6 @@ level = Level(level_map, screen)
 sky = pygame.image.load('pygameassets/backgroundimage.png')
 DEFAULT_SKY_SIZE = (1200, screen_height)
 sky = pygame.transform.scale(sky, DEFAULT_SKY_SIZE)
-
-scroll = [0, 0]
-
-
-
-
-
-
 
 #open loop area
 while True:
